212051e265f3aa9050a5e9bc2c9911af761d1c67
kiskovi97 <kiskovi97@gmail.com>
2017-11-10 19:30:18 +0100

    Csoportosítás

--- Converters/DepthProcesser.cpp
+++ Converters/DepthProcesser.cpp
@@ -63,26 +63,26 @@ void DepthProcesser::Process(uint16* depthfield, uint32* RGBfield,const int m_de
 		float mx = depthfield[i]*1.0f * ((x - (m_depthWidth / 2))*(1.0f) / (2.0f * m_Depth));
 
 		irany=irany.normalize();
-		pontok.push_back(cam.forward.normalize()*(depthfield[i] / 3.0f) + cam.right.normalize()*mx + cam.up.normalize()*my + cam.position);
+		vec3 pont = cam.forward.normalize()*(depthfield[i] / 3.0f) + cam.right.normalize()*mx + cam.up.normalize()*my + cam.position;
+		pont.x = (int)pont.x - (int)pont.x % 10;
+		pont.y = (int)pont.y - (int)pont.y % 10;
+		pont.z = (int)pont.z - (int)pont.z % 10;
+		
+		if (pont.z<1000) pontok.push_back(pont);
 		//pontok.push_back(irany*depthfield[i]+cam.position);
 		RGBfield[i] = 0x000000;//Convert(depthfield[i]);
 	}
-	
 
-	/*if (elore) nezo.position = nezo.position + vec3(5, 0, 0);
-	else nezo.position = nezo.position - vec3(5, 0, 0);
-	if (nezo.position.x > 400) elore = false;
-	if (nezo.position.x < -400) elore = true;*/
 	
 	for (vec3 pont : pontok) {
 		vec3 irany = pont - nezo.position;
 		vec3 pixel = nezo.position + irany*(dota(nezo.forward, nezo.forward) / dota(nezo.forward, irany));
 		float _x = dota(pixel - nezo.position, nezo.right) / (nezo.right.Length()*nezo.right.Length());
 		float _y = dota(pixel - nezo.position, nezo.up) / (nezo.up.Length()*nezo.up.Length());
-		int x = ((_x+0.25f) * 2.0f)*m_depthWidth;
-		int y = ((0.25f-_y) * 2.0f)*m_depthHeight;
+		int x = ((_x+0.25f) * 1.0f)*m_depthWidth;
+		int y = ((0.25f-_y) * 1.0f)*m_depthHeight;
 		if (x >= m_depthWidth || y >= m_depthHeight || x < 0 || y < 0){
-			//printf("Hus");
+			
 		}
 		else {
 			if (DepthMap[y*m_depthWidth + x]==0  || DepthMap[y*m_depthWidth + x] >  (pont - nezo.position).Length())
@@ -98,6 +98,7 @@ void DepthProcesser::Process(uint16* depthfield, uint32* RGBfield,const int m_de
 	
 	if (!pontok.empty() && file) {
 		WriteOut();
+		std::cout << "Written Out" << std::endl;
 		file = false;
 	}
 	pontok.clear();
